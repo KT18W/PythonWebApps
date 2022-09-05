8d24ecf9cffae4d07c6d9284082f4880789e2622
Katie <galaxyventurer2000@gmail.com>
2022-09-05 16:05:21 -0600

    create own Superhero directory and all contents

---
+++ Superhero/config/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from hero.views import BatmanView, SpidermanView, IndexView, GrootView
+
+urlpatterns = [
+    path('', IndexView.as_view()),
+    path('spiderman', SpidermanView.as_view()),
+    path('groot', GrootView.as_view()),
+    path('batman', BatmanView.as_view()),
+]
---
+++ Superhero/hero/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'heroes.html'
+
+# Create your views here.
+class SpidermanView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Spiderman',
+            'body': 'My name is Peter Parker',
+            'image': '/static/images/spiderman.jpg'
+        }
+
+
+class BatmanView(TemplateView):
+    template_name = "hero.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Batman',
+            'body': 'My name is Bruce Wayne',
+            'image': '/static/images/batman.jpg'
+        }
+
+
+class GrootView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Groot',
+            'body': 'I am Groot',
+            'image': '/static/images/groot.jpg'
+        }
