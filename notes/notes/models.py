c3f592d809848f87f220d9f5052bd9651a4c43b9
Katie <galaxyventurer2000@gmail.com>
2022-09-21 15:49:13 -0600

    add runtime and requriements to notes

--- notes/notes/models.py
+++ notes/notes/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Notes(models.Model):
-    title = models.CharField(max_length=200)
+    Hero = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     body = models.TextField()
 
     def __str__(self):
-        return f'{self.pk}. {self.title} - {self.author}'
+        return f'{self.pk}. {self.Hero} - {self.author}'
