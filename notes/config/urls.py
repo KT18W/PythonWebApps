71cf82c83523de15911473bc9e5d44cb61d7a06a
Katie <galaxyventurer2000@gmail.com>
2022-09-22 14:26:08 -0600

    get initial webpage up in front of the database

--- notes/config/urls.py
+++ notes/config/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from notes.views import NoteDetailView, NoteListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Note
+    path('', NoteListView.as_view()),
+    path('<int:pk>', NoteDetailView.as_view()),
 ]
--- notes/notes/views.py
+++ notes/notes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Notes
 
-# Create your views here.
+
+class NoteListView(TemplateView):
+    template_name = 'notes.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Notes.objects.all()
+        }
+
+
+class NoteDetailView(TemplateView):
+    template_name = 'note.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'note': Notes.objects.get(pk=kwargs['pk'])
+        }
