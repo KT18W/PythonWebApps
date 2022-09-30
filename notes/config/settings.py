78bc54cc5536acf836526856e2ad9b9adbb4747c
Katie <galaxyventurer2000@gmail.com>
2022-09-30 17:13:01 -0600

    try turining off debug and reset notes to note

--- notes/config/settings.py
+++ notes/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zxjlj5yas@in7zws5g(d@_@aq+kto1zf@^-tv9kw-_2k1gqt_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ondigitalocean.app']
 
 
 # Application definition
