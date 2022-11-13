c8289c152f0332c0a1e70ed644393e35ce1ca41a
Katie <galaxyventurer2000@gmail.com>
2022-11-13 13:12:14 -0700

    serialize/ download json works

--- Superhero/photos/models.py
+++ Superhero/photos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from django.core import serializers
+import json
+from json import dump
 
 class Hero(models.Model):
     name = models.CharField(max_length=200, default='name')
@@ -17,10 +20,21 @@ class Hero(models.Model):
         return reverse_lazy('hero_list')
 
 class Author(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
     bio = models.TextField()
+    #attempt at foreign key
+    #hero = models.ForeignKey(Hero, default='Anonymous', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.username}'
     
+#from django documentaion
+all_objects = [*Author.objects.all(), *Hero.objects.all()]
+data = serializers.serialize('json', all_objects)
+with open('data.json', 'w') as f:
+    dump(data, f, indent=4)
+#deserialize
+# with open('data.json', 'r') as f:
+#     for obj in serializers.deserialize('json', f):
+#         data = json.loads(obj)
+
